78ae6a294b9cd47948ee09573af753bd30295743
Daineka A.U <zanoza193@mail.ru>
2024-02-07 15:45:55 +0300

    Last Task

--- src/test/java/ru/netology/springbootdemoapp_task1/SpringBootDemoAppTask1ApplicationTests.java
+++ src/test/java/ru/netology/springbootdemoapp_task1/SpringBootDemoAppTask1ApplicationTests.java
@@ -1,13 +1,40 @@
 package ru.netology.springbootdemoapp_task1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoAppTask1ApplicationTests {
+    private static final String HOST  = "http://localhost:";
+    private static final String GET_MAPPING = "/model";
+    @Autowired
+    TestRestTemplate restTemplate;
+    public static GenericContainer<?> applicationDEV = new GenericContainer<>("devapp").
+            withExposedPorts(8080);
+    public static GenericContainer<?> applicationPROD = new GenericContainer<>("prodapp").
+            withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        applicationDEV.start();
+        applicationPROD.start();
+
+    }
 
     @Test
     void contextLoads() {
+        ResponseEntity<String> devApp = restTemplate.getForEntity(HOST + applicationDEV.getMappedPort(8080)+ GET_MAPPING, String.class);
+        Assertions.assertEquals("Current profile is dev", devApp.getBody());
+        ResponseEntity<String> prodApp = restTemplate.getForEntity(HOST + applicationPROD.getMappedPort(8081) + GET_MAPPING, String.class);
+        Assertions.assertEquals("Current profile is production", prodApp.getBody());
+
     }
 
 }
